0ba748c357660f84572680bed8ba2c26704ea74e
Jayesh Jangle <jayeshjangle50@gmail.com>
2024-12-21 19:44:22 +0530

    Montehall.py

--- montehall.py
+++ montehall.py
@@ -3,30 +3,33 @@ door=[0,0,0]
 goatdoor=[]
 swap=0
 dontswap=0
-x=random.randint(0,2)
-door[x]="BMW"
-for i in range(0,3):
-    if i==x:
-        continue
-    else:
-        door[i]="GOAT"
-        goatdoor.append("GOAT")
-choice=int(input("ENTER YOUR CHOICE :- "))
-door_open=random.choice(goatdoor)
-while door_open==choice:
+j=0
+while j<10:
+    x=random.randint(0,2)
+    door[x]="BMW"
+    for i in range(0,3):
+        if i==x:
+            continue
+        else:
+            door[i]="GOAT"
+            goatdoor.append("GOAT")
+    choice=int(input("ENTER YOUR CHOICE :- "))
     door_open=random.choice(goatdoor)
-ch=input("YOU WANT TO SWAP ! Y/N ")
-if ch=='y':
-    if door[choice]=='GOAT':
-        print("PLAYER WIN !!")
-        swap+=1
+    while door_open==choice:
+        door_open=random.choice(goatdoor)
+    ch=input("YOU WANT TO SWAP ! Y/N ")
+    if ch=='y':
+        if door[choice]=='GOAT':
+            print("PLAYER WIN !!")
+            swap+=1
+        else:
+            print(" PLAYER LOST !!")
     else:
-        print(" PLAYER LOST !!")
-else:
-    if door[choice]=='GOAT':
-        print("PLAYER LOST !!")
-    else:
-        print(" PLAYER WIN !!")
-        dontswap+=1
+        if door[choice]=='GOAT':
+            print("PLAYER LOST !!")
+        else:
+            print(" PLAYER WIN !!")
+            dontswap+=1
+    j+=1
 print(swap)
 print(dontswap)
